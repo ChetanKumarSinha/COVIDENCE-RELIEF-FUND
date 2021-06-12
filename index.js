55551008b1f651ca202413ad56ad6432142b39bf
CHETAN <chetansinha1404@gmail.com>
2021-06-12 21:02:33 +0530

    final payment commit

--- index.js
+++ index.js
@@ -1,27 +1,3 @@
-// window.onscroll = function () { scrollFunction() };
-
-// function scrollFunction() {
-//     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-//         document.getElementById("navbar").style.top = "0";
-//     } else {
-//         document.getElementById("navbar").style.top = "-100px";
-//     }
-// }
-
-// document.getElementById('main').addEventListener('click', closeNav);
-
-// function openNav() {
-//     document.getElementById("mySidenav").style.width = "200px";
-//     document.getElementById("main").style.marginLeft = "200px";
-// }
-
-// function closeNav() {
-//     document.getElementById("mySidenav").style.width = "0";
-//     document.getElementById("main").style.marginLeft = "0";
-//     document.body.style.backgroundColor = "white";
-// }
-
-
 
     const body = document.querySelector("body");
     const navbar = document.querySelector(".navbar");
